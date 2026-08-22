6c2027543500bf8ae20645bc2a18e3905f349bfd
raito-del <wazed7820@gmail.com>
2026-08-22 11:02:38 +0600

    Refactor build.gradle.kts structure
    Updated build.gradle.kts to include buildscript and allprojects sections.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,17 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.example.mylink"
-    compileSdk = 33
-
-    defaultConfig {
-        applicationId = "com.example.mylink"
-        minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
     }
 }
 
-dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }

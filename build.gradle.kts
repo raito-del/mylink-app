961d8491cecf7f6e50371e526de08547d88b0682
raito-del <wazed7820@gmail.com>
2026-08-22 10:35:39 +0600

    Configure Android application with SDK and dependencies
    Updated Android and Kotlin plugin versions, added Android configuration and dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,31 @@
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.mylink"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.mylink"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
 }
